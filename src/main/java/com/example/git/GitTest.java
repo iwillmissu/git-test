28cc896dc286c67c44860cf8eb2badfe0b476f8f
xuhu <1366615739@qq.com>
2022-11-24 13:30:47 +0800

    FIRST COMMIT

---
+++ src/main/java/com/example/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.example.git;
+
+/**
+ * description: GitTest
+ * date: 2022/11/24 12:30
+ * author: xuhu
+ * version: v1.0
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
