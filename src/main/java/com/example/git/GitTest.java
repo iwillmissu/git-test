0c9a6495b9dc5f8e9f4ba6fb59cac6886fb44b11
xuhu <1366615739@qq.com>
2022-11-24 13:51:02 +0800

    hot-fix commit2

--- src/main/java/com/example/git/GitTest.java
+++ src/main/java/com/example/git/GitTest.java
@@ -13,6 +13,7 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hot-fix git");
     }
 
 }
