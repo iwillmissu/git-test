7145d3584cab99f5ec3fd77d0a7d10df99db252e
xuhu <1366615739@qq.com>
2022-11-24 14:24:46 +0800

    git-push commit

--- src/main/java/com/example/git/GitTest.java
+++ src/main/java/com/example/git/GitTest.java
@@ -15,6 +15,7 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("master git");
         System.out.println("hot-fix git");
+        System.out.println("push git");
     }
 
 }
